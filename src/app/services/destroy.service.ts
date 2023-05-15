29edcb42337338ac3edebadbc0c907768b48415f
Borislav <95305283+Handehoch@users.noreply.github.com>
2023-05-15 07:05:37 +0500

    Update destroy.service.ts

--- src/app/services/destroy.service.ts
+++ src/app/services/destroy.service.ts
@@ -1,10 +1,6 @@
 import { Subject } from 'rxjs';
 import { Injectable, OnDestroy } from '@angular/core';
 
-/**
- * @see https://github.com/TinkoffCreditSystems/taiga-ui
- * Observable abstraction over ngOnDestroy to use with takeUntil
- */
 @Injectable()
 export class DestroyService extends Subject<void> implements OnDestroy {
     ngOnDestroy() {
